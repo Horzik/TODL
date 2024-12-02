f4b694f9aec8c205b2d247117c5955f658215255
Horzik <horzik77@gmail.com>
2024-12-02 02:54:05 +0100

    Something something

--- todl.py
+++ todl.py
@@ -2,11 +2,12 @@
 import json
 import os.path
 import argparse
-
+import unicodedata
 
 from todl.config import TODL_FP
 from todl.config import REVERZ_FP
 from todl.config import INVERT_FP
+
 
 def write(records: list[str]) -> None:
     with open(TODL_FP, "w") as f:
@@ -34,7 +35,6 @@ def log_action(action):
 
     with open(REVERZ_FP, "w") as a:
         json.dump(history, a, indent=4)
-
 
 
 def add_todl(args: list[str]) -> None:
@@ -130,10 +130,8 @@ def list_records():
 
 
 def undo():
-
     with open(REVERZ_FP, "r") as f:
         history = json.load(f)
-
 
         if not history:
             print("Lmao tu nic neni")
@@ -155,21 +153,92 @@ def undo():
         removed_indexes = last_action["Index"]
         removed_tasks = last_action["Task"]
 
-        for index, task in zip (sorted(removed_indexes), removed_tasks):
-
-            index = index - 1
-            if 0 <= index <= len(records):
-                records.insert(index -1, task + "\n")
+        for index, task in sorted(zip(removed_indexes, removed_tasks)):
+            if 0 <= index <= len(records) + 1:
+                records.insert(index - 1, task + "\n")
             else:
                 print(f"Error: Invalid index {index} for reinsertion.")
                 return
 
         print(f"Added '{removed_tasks}' at position {removed_indexes}")
 
+    if last_action["Action"] == "doner":
+        for index, task in zip(sorted(last_action["Index"]), last_action["Task"]):
+            index = index - 1
+            if 0 <= index <= len(records):
+                records.pop(index)
+                records.insert(index, task + "\n")
+                print(f"Beef with {index + 1} resolved")
+            else:
+                print(f"Error: Invalid index {index} for reinsertion.")
+                return
+
     with open(REVERZ_FP, "w") as f:
         json.dump(history, f, indent=4)
 
     write(records)
+
+
+def doner(val):
+    records = read()
+    action = {"Action": "doner", "Task": "", "Index": 0}
+
+    if not records:
+        print("U wot m8")
+        return
+
+    try:
+        if val == "f":
+            indexes = [0]
+        elif val == "l":
+            indexes = [len(records) - 1]
+        elif ":" in val:
+            start, end = map(int, val.split(":"))
+            if start > end:
+                print(f"Error: {start} is larger than {end}")
+                return
+            indexes = list(range(start - 1, end))
+        elif "," in val:
+            indexes = [int(i) - 1 for i in val.split(",")]
+        elif val == "a":
+            confirmation = input("Bruh you really wanna blast 'em? (y/n): ")
+            if confirmation.lower() != 'y':
+                print("SAVED")
+                return
+            indexes = [i for i in range(len(records))]
+        elif val == "d":
+            indexes = [i for i in range(len(records))]
+            for index in indexes:
+                if 0 <= index < len(records):
+                    task = records[index].strip()
+                    cleaned_task = ''.join(char for char in task if unicodedata.category(char) != 'Mn')
+                    records[index] = cleaned_task + "\n"
+            write(records)
+            print(f"Made peace with the whole city")
+            return
+        else:
+            indexes = [int(val) - 1]
+
+        crossed_indexes = []
+        crossed_tasks = []
+
+        for index in indexes:
+            if 0 <= index < len(records):
+                task = ' ' + records[index].strip()
+                crossed_task = ''.join([char + '\u0336' for char in task])
+                crossed_tasks.append(records[index].strip())
+                records[index] = crossed_task + '\n'
+                crossed_indexes.append(index + 1)
+
+        write(records)
+        print(f"You krossed these {crossed_indexes} fools ")
+
+        action["Task"] = crossed_tasks
+        action["Index"] = crossed_indexes
+        log_action(action)
+
+    except ValueError:
+        print("Mackas mi hada")
 
 
 def init_argparse():
@@ -226,6 +295,14 @@ Use 'a' to clear all listings'''
         help="WIP, nothing to see here"
     )
 
+    p.add_argument(
+        "--done",
+        "-d",
+        "-k",
+        action="store",
+        help="For krossing these fools"
+    )
+
     return p
 
 
@@ -245,32 +322,40 @@ if __name__ == "__main__":
     if parsed_args.undo:
         undo()
 
+    if parsed_args.done:
+        doner(parsed_args.done)
 
 """
 1) del - check array bounds : CHECKERZ
 2) remove first / last element : CHECKERZ
 3) handle arg clash e.g. remove / add / list together : CHECKERZ except ???
-/ right now it first Removes, then Adds 
+/first Removes, then Adds 
 / dunno if that's sufficient
 4) clear with user y/n input : CHECKERZ
 5) insert on position : CHECKERZ except KILLMYSELF
-/ it works but not as I intended ( -a42 )
+/ it works but not as *I want* ( -a42 )
 / to insert on position type "-a "42 content"
 / LEAVE BRITTNEY ALONE
 6) zerver : NOPERZ
-7) PORNHUB ehhhhh I mean GITHUB : NOPERZ
+7) PORNHUB ehhhhh I mean GITHUB : CHECKERZ
+
 XTRAS
+
 8) adding multiple words as one wrekkord : CHECKERZ
 / (" ".join())
 9) help ain't printing right : CHECKERZ 
 / (formatter_class)
 10) ADD - if nic then print NOPERZ 
 / prints arg help, why not the print inside the add function?
-11) UNDO - fml
+/ clueless
+11) UNDO - maybe CHECKERZ???
+12) doner kross mania CHECKERZ
 NOTES
 -- delete "reverse=True" otherwise RIP
     // ok but prints in reverse (duh)
     // reversing back again for printing is sus
---- 
-
+-- 3 != 3 aka 0 based
+    // not very based
+-- NOT changing documentation
+-- redo soon???
 """
